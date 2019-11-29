7816b0e35bbb61c88e1f954c49afb5ec3bde4502
Vinay Sharma <vinay04sharma@icloud.com>
2019-11-29 20:32:40 +0530

    fix(bug); update hardcoded repo path

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,12 +14,12 @@ echo '{
     "email": "tectonic-action@github.com"
   },
   "content": "'"$(base64 -w 0 $OUTPUT_PDF)"'",
-  "sha": '$(curl -X GET https://api.github.com/repos/vinay0410/webpage/contents/$OUTPUT_PDF | jq .sha)'
+  "sha": '$(curl -X GET -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/${GITHUB_REPOSITORY}/contents/$OUTPUT_PDF | jq .sha)'
 }' >> payload.json
 
 STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" \
             -i -X PUT -H "Authorization: token $GITHUB_TOKEN" -d @payload.json \
-            https://api.github.com/repos/vinay0410/webpage/contents/$OUTPUT_PDF)
+            https://api.github.com/repos/${GITHUB_REPOSITORY}/contents/$OUTPUT_PDF)
 
 if [ $((STATUSCODE/100)) -ne 2 ]; then
   echo "Github's API returned $STATUSCODE"
